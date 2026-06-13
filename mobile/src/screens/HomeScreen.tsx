a320d376e2c23e2af1e4b82b797bb09698397fb1
jhingalala-a <julisingh73209@gmail.com>
2026-06-13 21:37:59 +0530

    Premium home screen UI improvements

--- mobile/src/screens/HomeScreen.tsx
+++ mobile/src/screens/HomeScreen.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-
+import { Image } from "react-native";
 import {
   SafeAreaView,
   Text,
@@ -138,7 +138,27 @@ export default function HomeScreen({ navigation }: any) {
       console.log(error);
     }
   };
+  const getFolderColor = (name: string) => {
+    switch (name.toLowerCase()) {
+      case "photos":
+        return "#5E5BE6";
 
+      case "videos":
+        return "#5D76CB";
+
+      case "documents":
+        return "#53895F";
+
+      case "instagram":
+        return "#E35AA5";
+
+      case "pinterest":
+        return "#E35AA5";
+
+      default:
+        return "#7C3AED";
+    }
+  };
   const refreshFolderStats = () => {
 
     fetch("http://10.0.2.2:3000/folders/stats")
@@ -185,25 +205,50 @@ export default function HomeScreen({ navigation }: any) {
     <SafeAreaView style={styles.container}>
       <ScrollView showsVerticalScrollIndicator={false}>
 
-        <Text style={styles.logo}>🔒</Text>
+        <View style={styles.header}>
+          <View style={styles.menuBtn}>
+            <Text style={styles.headerIcon}>☰</Text>
+          </View>
+          <Text style={styles.logoText}>
+            Vaultly
+          </Text>
+          <View style={styles.profileBtn}>
+            <Text style={styles.headerIcon}>👤</Text>
+          </View>
+        </View>
 
-        <Text style={styles.title}>
-          Vaultly
+        <Text style={styles.goodMorning}>
+          Good Morning 👋
         </Text>
 
-        <Text style={styles.subtitle}>
-          Welcome Back 👋
+        <Text style={styles.welcome}>
+          Welcome{" "}
+          <Text style={styles.welcomePurple}>
+            Back
+          </Text>
         </Text>
 
         <Text style={styles.tagline}>
           Your memories, organized and searchable.
         </Text>
 
-        <TextInput
-          placeholder="🔍 Search anything..."
-          placeholderTextColor="#94A3B8"
-          style={styles.search}
-        />
+        <View style={styles.search}>
+          <Image
+            source={require("./icons/search.png")}
+            style={{
+              width: 24,
+              height: 24,
+            }}
+          />
+
+          <TextInput
+            placeholder="Search anything..."
+            placeholderTextColor="#94A3B8"
+            style={styles.searchInput}
+          />
+
+          <Text style={styles.searchIcon}>✨</Text>
+        </View>
 
         <Text style={styles.sectionTitle}>
           Quick Actions
@@ -215,31 +260,63 @@ export default function HomeScreen({ navigation }: any) {
             style={styles.quickCard}
             onPress={loadGallery}
           >
-            <Text style={styles.quickIcon}>🖼️</Text>
-            <Text style={styles.quickTitle}>Photos</Text>
+            <View style={styles.iconBoxPurple}>
+              <Image
+                source={require("./icons/photo.png")}
+                style={{
+                  width: 24,
+                  height: 24,
+                }}
+              />
+            </View>
+
+            <Text style={styles.quickTitle}>
+              Photos
+            </Text>
           </TouchableOpacity>
 
           <TouchableOpacity
             style={styles.quickCard}
             onPress={loadVideos}
           >
-            <Text style={styles.quickIcon}>🎥</Text>
-            <Text style={styles.quickTitle}>Videos</Text>
+            <View style={styles.iconBoxBlue}>
+              <Image
+                source={require("./icons/video.png")}
+                style={{
+                  width: 24,
+                  height: 24,
+                }}
+              />
+            </View>
+
+            <Text style={styles.quickTitle}>
+              Videos
+            </Text>
           </TouchableOpacity>
 
           <TouchableOpacity
             style={styles.quickCard}
             onPress={loadPdfs}
           >
-            <Text style={styles.quickIcon}>📄</Text>
-            <Text style={styles.quickTitle}>Documents</Text>
+            <View style={styles.iconBoxGreen}>
+              <Text style={styles.quickIcon}>📄</Text>
+            </View>
+
+            <Text style={styles.quickTitle}>
+              Documents
+            </Text>
           </TouchableOpacity>
 
           <TouchableOpacity
             style={styles.quickCard}
           >
-            <Text style={styles.quickIcon}>🔗</Text>
-            <Text style={styles.quickTitle}>Save Link</Text>
+            <View style={styles.iconBoxPink}>
+              <Text style={styles.quickIcon}>🔗</Text>
+            </View>
+
+            <Text style={styles.quickTitle}>
+              Save Link
+            </Text>
           </TouchableOpacity>
 
         </View>
@@ -265,25 +342,55 @@ export default function HomeScreen({ navigation }: any) {
 
             <View style={styles.folderCard}>
 
-              <View>
+              <View
+                style={{
+                  flexDirection: "row",
+                  alignItems: "center",
+                }}
+              >
 
-                <Text style={styles.folderName}>
-                  {getFolderIcon(folder.name)} {folder.name}
-                </Text>
+                <View
+                  style={[
+                    styles.folderIconBox,
+                    {
+                      backgroundColor: getFolderColor(folder.name),
+                    },
+                  ]}
+                >
+                  <Text style={styles.folderIcon}>
+                    {getFolderIcon(folder.name)}
+                  </Text>
+                </View>
 
-                <Text style={styles.folderSub}>
-                  All your {folder.name.toLowerCase()}
-                </Text>
+                <View>
+                  <Text style={styles.folderName}>
+                    {folder.name}
+                  </Text>
 
+                  <Text style={styles.folderSub}>
+                    All your {folder.name.toLowerCase()}
+                  </Text>
+                </View>
               </View>
 
-              <View style={styles.badge}>
-                <Text style={styles.badgeText}>
-                  {
-                    folderStats.find(
-                      (f) => f.id === folder.id
-                    )?.count ?? 0
-                  }
+              <View
+                style={{
+                  flexDirection: "row",
+                  alignItems: "center",
+                }}
+              >
+                <View style={styles.badge}>
+                  <Text style={styles.badgeText}>
+                    {
+                      folderStats.find(
+                        (f) => f.id === folder.id
+                      )?.count ?? 0
+                    }
+                  </Text>
+                </View>
+
+                <Text style={styles.arrow}>
+                  →
                 </Text>
               </View>
 
@@ -294,6 +401,9 @@ export default function HomeScreen({ navigation }: any) {
         ))}
 
       </ScrollView>
+      <TouchableOpacity style={styles.fab}>
+        <Text style={styles.fabText}>+</Text>
+      </TouchableOpacity>
     </SafeAreaView>
   );
 }
@@ -302,9 +412,135 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     padding: 20,
-    backgroundColor: "#0F172A",
+    backgroundColor: "#050816",
+  },
+  folderIconBox: {
+    width: 46,
+    height: 46,
+    borderRadius: 9,
+    backgroundColor: "#6D4AFF",
+
+    justifyContent: "center",
+    alignItems: "center",
+
+    marginRight: 14,
   },
 
+  folderIcon: {
+    fontSize: 22,
+  },
+  fab: {
+    position: "absolute",
+
+    right: 20,
+    bottom: 10,
+
+    width: 54,
+    height: 54,
+
+    borderRadius: 32,
+
+    backgroundColor: "#7C3AED",
+
+    justifyContent: "center",
+    alignItems: "center",
+
+    elevation: 12,
+  },
+  logoText: {
+    color: "#FFFFFF",
+    fontSize: 24,
+    fontWeight: "500",
+  },
+
+  fabText: {
+    color: "#FFFFFF",
+    fontSize: 32,
+    fontWeight: "300",
+  },
+  iconBoxPurple: {
+    width: 44,
+    height: 44,
+    borderRadius: 10,
+    backgroundColor: "#5E5BE6",
+    justifyContent: "center",
+    alignItems: "center",
+    marginBottom: 12,
+  },
+
+  iconBoxBlue: {
+    width: 44,
+    height: 44,
+    borderRadius: 10,
+    backgroundColor: "#5D76CB",
+    justifyContent: "center",
+    alignItems: "center",
+    marginBottom: 12,
+  },
+
+  iconBoxGreen: {
+    width: 44,
+    height: 44,
+    borderRadius: 10,
+    backgroundColor: "#53895F",
+    justifyContent: "center",
+    alignItems: "center",
+    marginBottom: 12,
+  },
+
+  iconBoxPink: {
+    width: 44,
+    height: 44,
+    borderRadius: 10,
+    backgroundColor: "#A13E74",
+    justifyContent: "center",
+    alignItems: "center",
+    marginBottom: 12,
+  },
+
+  header: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginTop: 20,
+  },
+
+  menuBtn: {
+    width: 45,
+    height: 45,
+    borderRadius: 14,
+    backgroundColor: "#111827",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+
+  profileBtn: {
+    width: 45,
+    height: 45,
+    borderRadius: 14,
+    backgroundColor: "#7C3AED",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+
+
+  headerIcon: {
+    color: "#FFF",
+    fontSize: 20,
+  },
+
+  goodMorning: {
+    color: "#A1A1AA",
+    marginTop: 20,
+    fontSize: 15,
+  },
+
+  welcome: {
+    color: "#FFFFFF",
+    fontSize: 36,
+    fontWeight: "700",
+    marginTop: 6,
+  },
   logo: {
     fontSize: 60,
     textAlign: "center",
@@ -318,28 +554,38 @@ const styles = StyleSheet.create({
     color: "#FFFFFF",
   },
 
-  subtitle: {
-    textAlign: "center",
-    color: "#FFFFFF",
-    fontSize: 26,
-    fontWeight: "700",
-    marginTop: 15,
-  },
 
   tagline: {
-    textAlign: "center",
-    color: "#94A3B8",
+    textAlign: "left",
+    color: "#7C8AA5",
     marginTop: 8,
     marginBottom: 25,
     fontSize: 15,
   },
 
   search: {
-    backgroundColor: "#1E293B",
-    borderRadius: 18,
-    padding: 16,
+    backgroundColor: "#1B2438",
+    borderRadius: 30,
+    height: 62,
+
+    flexDirection: "row",
+    alignItems: "center",
+
+    paddingHorizontal: 18,
+
+    marginTop: 20,
+    marginBottom: 30,
+  },
+
+  searchInput: {
+    flex: 1,
     color: "#FFFFFF",
-    marginBottom: 25,
+    fontSize: 16,
+    marginHorizontal: 10,
+  },
+
+  searchIcon: {
+    fontSize: 22,
   },
 
   sectionTitle: {
@@ -357,23 +603,35 @@ const styles = StyleSheet.create({
   },
 
   quickCard: {
-    width: "48%",
-    backgroundColor: "#1E293B",
-    borderRadius: 20,
-    paddingVertical: 25,
+    width: "23%",
+
+    backgroundColor: "#151F35",
+
+    borderRadius: 10,
+
+    height: 117,
+
+    justifyContent: "center",
     alignItems: "center",
-    marginBottom: 12,
   },
 
   quickIcon: {
-    fontSize: 28,
-    marginBottom: 10,
+    fontSize: 20,
+    textAlign: "center",
+    textAlignVertical: "center",
+    marginBottom: 4,
   },
 
   quickTitle: {
     color: "#FFFFFF",
-    fontSize: 16,
-    fontWeight: "600",
+    fontSize: 13,
+    fontWeight: "400",
+    textAlign: "center",
+
+    marginTop: 10,
+  },
+  welcomePurple: {
+    color: "#6f38f1",
   },
 
   folderCard: {
@@ -382,8 +640,8 @@ const styles = StyleSheet.create({
     shadowRadius: 12,
     elevation: 5,
     backgroundColor: "#1E293B",
-    borderRadius: 18,
-    padding: 20,
+    borderRadius: 12,
+    padding: 12,
     marginBottom: 12,
     flexDirection: "row",
     justifyContent: "space-between",
@@ -401,11 +659,19 @@ const styles = StyleSheet.create({
     fontSize: 13,
   },
 
+
   badge: {
-    backgroundColor: "#7C3AED",
-    paddingHorizontal: 12,
-    paddingVertical: 6,
-    borderRadius: 20,
+    backgroundColor: "#3F3B77",
+
+    minWidth: 50,
+    height: 30,
+
+    paddingHorizontal: 16,
+
+    borderRadius: 15,
+
+    justifyContent: "center",
+    alignItems: "center",
   },
 
   badgeText: {
@@ -416,8 +682,8 @@ const styles = StyleSheet.create({
 
 
   arrow: {
-    color: "#8B5CF6",
-    fontSize: 22,
-    fontWeight: "bold",
+    color: "#FFFFFF",
+    fontSize: 24,
+    marginLeft: 12,
   },
 });
